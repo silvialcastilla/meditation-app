6d716bed7780f2074d2618313b14f6e44ee5ab77
silvialcastilla <silope0302@gmail.com>
2021-09-15 13:05:10 +0200

    added

--- src/pages/TopicPage/TopicPage.jsx
+++ src/pages/TopicPage/TopicPage.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import { Description } from '../../components/Description/Description'
 import { CardList} from '../../components/CardList/CardList'
 
-import "leaflet/dist/leaflet.css";
 
 import data from '../../utils/data.json'
 
 export const TopicPage = () => {
     return (
-        <div>
+        <div className="topic">
             <Description
-                className="description-topic"
+                className="title-topic"
                 information="choose a topic to focuse on:"
                 title="What Brings you to Silent Moon?"
             />
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import { Title } from '../../components/Title/Title';
 import { Image } from '../../components/Image/Image';
+import { useHistory } from "react-router-dom";
+
+
 import { Description } from '../../components/Description/Description';
 import { Button } from '../../components/Button/Button';
 
 import meditation from '../../assets/meditation.png'
 
 export const WelcomePage = () => {
+    const history = useHistory();
+    
     return (
         <div className="welcome">
+        <Button
+                alt="logout"
+                className="button-logout"
+                onClick={() => history.push("/")}
+                title="logout"
+        />
         <Title
             className="title-welcome"
             title="Silent Moon"
@@ -25,10 +36,10 @@ export const WelcomePage = () => {
             src={meditation}
         />
         <Button
-            className="button-welcome"
+            className="button-welcome-start"
+            onClick={() => history.push("/topic")}
             title="GET STARTED"
         />
-    
         </div>
     )
 }
